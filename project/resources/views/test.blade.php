84199213036092ea4425c6f31e0ee32e6a5f4441
Matt-Hackerman <mmhack916@yahoo.com>
2023-03-03 15:50:02 -0500

    small changes ^^^^^

--- project/resources/views/test.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('home')
-
-@section('header')
-@endsection
-
-@section('content')
-    <h1>test Content</h1>
-@endsection
--- project/routes/web.php
+++ project/routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
+Route::get('/header', function () {
+    return view('header');
 });
 
-Route::get('/test', function () {
-    return view('test');
+Route::get('/home', function () {
+    return view('home');
 });
