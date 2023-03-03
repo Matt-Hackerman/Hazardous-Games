9733af1c2a395a61b93b9814b5b04bac114c5157
Connor-Csoky <connorcsoky@gmail.com>
2023-03-03 13:07:10 -0500

    Created basic signup design

---
+++ project/resources/views/signup.blade.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>signup</title>
+        <link rel="stylesheet" href="signup.css">
+    </head>
+    <body>
+        <main class="main">
+            <div class="signupContainer">
+                <h1>Create Account</h1>
+                <input type="text" placeholder="First Name" required>
+                <input type="text" placeholder="Last Name" required>
+                <input type="text" placeholder="Username" required>
+                <input type="email" placeholder="Email" required>
+                <input type="password" placeholder="Password" required>
+                <div>
+                    <input type="button" value="Create">
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
