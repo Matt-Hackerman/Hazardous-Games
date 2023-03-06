3c64b727d6f92a219d758f8f7dd5344dc9d708b1
ECRAMi <64109619+ECRAMi@users.noreply.github.com>
2023-03-06 14:17:59 -0500

    Update api.php

--- project/routes/api.php
+++ project/routes/api.php
@@ -1,12 +1,18 @@
 <?php
 
-use App\Http\Controllers\UsersController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
 
-
-
-route::resource('users', UsersController::class);
-
-?>
+Route::post('/login', [AuthController::class, 'login']);
