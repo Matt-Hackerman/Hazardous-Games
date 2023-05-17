89562834310c7a5fb097178c2802498eabd73e19
Matt-Hackerman <mmhack916@yahoo.com>
2023-05-17 15:15:36 -0400

    added random ads

--- project/public/JavaScript/main.js
+++ project/public/JavaScript/main.js
@@ -9,3 +9,26 @@ $('#siteTheme').click(function() {
         $('body').addClass("light");
     }
 });
+
+let fillerArr = [
+    "../images/filler1.jpg",
+    "../images/filler2.png",
+    "../images/filler3.jpg",
+    "../images/filler4.png",
+    "../images/filler5.png",
+    "../images/filler6.jpg",
+    "../images/filler7.jpg",
+    "../images/filler8.jpg",
+    "../images/filler9.jpg",
+    "../images/filler10.jpg",
+    "../images/filler11.jpg",
+    "../images/filler12.jpeg"
+];
+
+$( document ).ready(function() {
+    for (let i = 1; i <= 4; i++) {
+        let ranFill = Math.floor(Math.random() * 12);
+        $('#f' + i).attr('src', fillerArr[ranFill]);
+        console.log(ranFill);
+    }
+});
